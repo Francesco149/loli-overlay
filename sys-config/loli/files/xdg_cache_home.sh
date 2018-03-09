2f78c55c61042a0d8e70a0d7e39a1ed039cabcfe
Franc[e]sco <lolisamurai@tfwno.gf>
2018-03-09 03:36:08 +0100

    sys-config/loli: move winetricks cache to home

--- sys-config/loli/files/xdg_cache_home.sh
+++ sys-config/loli/files/xdg_cache_home.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 export XDG_CACHE_HOME="/tmp/${USER}/.cache"
+mkdir -p "${XDG_CACHE_HOME}" >/dev/null 2>&1
+ln -sf "${HOME}/.winetricks-cache/" "${XDG_CACHE_HOME}/winetricks" \
+   >/dev/null 2>&1
